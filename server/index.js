774b8db42ec1fc8d311317b67cd6b25434471d77
Shreyachavan02 <chavanshreya654@gmail.com>
2025-11-11 17:44:50 +0530

    basic server is ready

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    if (conn) {
+      console.log("MongoDB connected successfully");
+    }
+  }catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Blogify API !",
+
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
