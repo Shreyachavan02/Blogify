77af8419c609bf40fcde8f0c90f54aa9d1ef7fce
Shreyachavan02 <chavanshreya654@gmail.com>
2025-11-12 21:20:45 +0530

    AddedSignup validation(name,email,password required before login)

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,34 @@ import User from "./../models/user.js";
 const postSignup = async (req, res) => {
 const { name, email, password } = req.body;
 
+if(!name) {
+return res.status(400).json({
+    success: false,
+    message: "Name is required",
+});
+}
+if(!email) {
+    return res.status(400).json({
+        success: false,
+        message: "Email is required",
+    });
+}
+if(!password) {
+    return res.status(400).json({
+        success: false,
+        message: "Password is required",
+    });
+}
+
+const existingUser = await User.findOne({ email });
+if (existingUser) {
+    return  res.status(400).json({
+        success: false,
+        message: `User with this email  ${email} already exists`,
+    });
+}
+
+
 const newUser = new User({ name, email, password });
 
 const savedUser = await newUser.save();
