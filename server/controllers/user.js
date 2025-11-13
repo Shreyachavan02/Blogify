abcaedf9d5daa67bbedfe51735726fc08b7ed8f4
Shreyachavan02 <chavanshreya654@gmail.com>
2025-11-13 10:55:15 +0530

    Regex pattern added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,22 +3,34 @@ import User from "./../models/user.js";
 const postSignup = async (req, res) => {
 const { name, email, password } = req.body;
 
-if(!name) {
-return res.status(400).json({
-    success: false,
-    message: "Name is required",
-});
-}
-if(!email) {
+if (!name || !email || !password) {
     return res.status(400).json({
         success: false,
-        message: "Email is required",
+        message: "Name, email, and password are required",
     });
 }
-if(!password) {
+
+const emailValidationRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const nameValidationRegex = /^[a-zA-Z ]+$/;
+const passwordValidationRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+if(nameValidationRegex.test(name) === false){
+return res.status(400).json({
+    success: false,
+    message: "Name should contain only alphabets and spaces",
+});
+}
+if(emailValidationRegex.test(email) == false){
     return res.status(400).json({
         success: false,
-        message: "Password is required",
+        message: "Email is not valid",
+    });
+}
+if(passwordValidationRegex.test(password) === false){ {
+    return res.status(400).json({
+        success: false,
+        message: "password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character",
     });
 }
 
@@ -41,6 +53,7 @@ res.json({
     user: savedUser,
 
  });
+};
 };
 const postLogin = (req, res) => {};
 
